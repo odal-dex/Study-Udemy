733686cba6060e19b930ad206120041d3a2b87f7
Alexey <odal-dex@mail.ru>
2023-03-05 11:49:40 +0300

    1. syntax error has been fixed (promt to prompt) 2. I studied until the 18th lesson. 3. The prompt function is provided by the browser, and you run the script not in the browser, but in NodeJS.~

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +promt('Сколько фильмов вы уже посмотрели?', ''); // Добавить плюс что бы данные от пользователя были в цифрах
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,13 +10,67 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = promt ('Один из последних просмотренных фильмов?', '');
-const b = promt ('На сколько вы оцените его?', '');
-const c = promt ('Один из последних просмотренных фильмов?', '');
-const d = promt ('На сколько вы оцените его?', '');
+const a = prompt ('Один из последних просмотренных фильмов?', ''),
+			b = prompt ('На сколько вы оцените его?', ''),
+			c = prompt ('Один из последних просмотренных фильмов?', ''),
+			d = prompt ('На сколько вы оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 console.log(numberOfFilms);
+
+
+
+
+
+
+
+//УЛОВИЯ
+
+/* let num = 50;
+while (num <= 55) {
+	console.log(num);
+	num++;
+} */
+/* for (let i = 0; i < 3; i++) {
+	console.log(i);
+	for (let j = 0; j < 3; j++) {
+		console.log(j);
+	}
+} */
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+/* let result = '';
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+	
+	for (let j = 0; j < i; j++) {
+		result += "*";
+	}
+	result += '\n';
+} 
+
+console.log(result); */
+
+// let result = '';
+// const lenght = 7;
+
+/* first: for (let i = 0; i < 3; i++) {
+	console.log(`First level: ${i}`);
+	for (let j = 0; j < 3; j++) {
+		console.log(`Second level: ${j}`);
+		for (let k = 0; k < 3; k++) {
+			if (k === 2) continue first;
+			console.log(`Third level: ${k}`);
+		}
+	}
+} */
