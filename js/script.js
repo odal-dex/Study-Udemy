42156af704ef12cff27d8d8e8f983a19f825665e
Alexey <odal-dex@mail.ru>
2023-03-10 22:44:22 +0300

    practice 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	} 
+}
+
+start();
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,15 +20,16 @@ const personalMovieDB = {
 	privat: false
 };
 
-/* const a = prompt ('Один из последних просмотренных фильмов?', ''),
+const a = prompt ('Один из последних просмотренных фильмов?', ''),
 			b = prompt ('На сколько вы оцените его?', ''),
 			c = prompt ('Один из последних просмотренных фильмов?', ''),
 			d = prompt ('На сколько вы оцените его?', '');
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; */
+personalMovieDB.movies[c] = d;
 
-first: for (let i = 0; i < 2; i++) {
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
 	const a = prompt ('Один из последних просмотренных фильмов?', ''),
 				b = prompt ('На сколько вы оцените его?', '');
 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
@@ -29,20 +40,41 @@ first: for (let i = 0; i < 2; i++) {
 		i--;
 	}
 }
-
-if (personalMovieDB.count < 10) {
-	console.log('Просмотренно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-	console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-	console.log('Вы киноман');
-} else {
-	console.log('Произошла ошибка');
 }
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотренно мало фильмов');
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log('Вы классический зритель');
+	} else if (personalMovieDB.count >= 30) {
+		console.log('Вы киноман');
+	} else {
+		console.log('Произошла ошибка');
+	}
+}
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
 
+function showMyDB (hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
 
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = promt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
+
+writeYourGenres();
 
 
 //УЛОВИЯ
@@ -92,3 +124,42 @@ console.log(result); */
 		}
 	}
 } */
+
+// * Функции
+
+
+/* function showFirstMessage (text) {
+	console.log(text);
+}
+
+showFirstMessage('Hello World');  */
+
+/* function calc(a, b) {
+	return (a + b);
+}
+console.log(calc(4, 3)); */
+
+/* function ret () {
+	let num =50;
+	return num;
+}
+const anotherNum = ret ();
+console.log(anotherNum); */
+
+// * Методы и свойства у строк и чисел
+
+/* const str = "test";
+const arr = [1, 2, 4];
+
+console.log(str.length);
+console.log(arr.length); */
+
+/* const logg = 'Hello world';
+console.log(logg.substr(7, 5)); */
+
+/* const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test)); */
